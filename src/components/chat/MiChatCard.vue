eaf255c709575d3f102c4a847c520f1b2f25feb7
Korimas <himoker@163.com>
2024-03-02 20:44:04 +0800

    update

--- src/components/chat/MiChatCard.vue
+++ src/components/chat/MiChatCard.vue
@@ -11,7 +11,7 @@
                 </q-tooltip></q-icon>
         </div>
         <div style="margin-top: 5px">
-            <q-circular-progress v-if="!Content" indeterminate size="40px" :thickness="0.4" font-size="50px" color="lime"
+            <q-circular-progress v-if="!Content" indeterminate size="xs" :thickness="0.4" font-size="50px" color="lime"
                 track-color="grey-3" center-color="grey-8" class="q-ma-md" />
             <div v-else>
                 {{ Content }}
