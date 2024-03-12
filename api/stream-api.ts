38c7a5d55f80860fbcebaaac4e950725959b802c
Korimas <himoker@163.com>
2024-03-12 18:02:07 +0800

    update

--- api/stream-api.ts
+++ api/stream-api.ts
@@ -2,6 +2,27 @@ import {RequestStream, GPTAPIMessage, GPTAPIRequest} from '../lib/openai/api';
 
 export const config = {
   runtime: 'edge',
+  // https://vercel.com/docs/edge-network/regions#region-list
+  regions: [
+      'arn1',
+      'bom1',
+      'cdg1',
+      'cle1',
+      'cpt1',
+      'dub1',
+      'fra1',
+      'gru1',
+      // 'hkg1', ip blocked by openai
+      'hnd1',
+      'iad1',
+      'icn1',
+      'kix1',
+      'lhr1',
+      'pdx1',
+      'sfo1',
+      'sin1',
+      'syd1',
+  ],
 };
 
 const handler = async (req: Request): Promise<Response> => {
