f59657f8e39b1ce48add16efc1c8d12b583178d2
Korimas <himoker@163.com>
2024-12-11 10:50:01 +0800

    update

--- src/components/header/ProjectDropDown.vue
+++ src/components/header/ProjectDropDown.vue
@@ -1,36 +1,70 @@
 <template>
   <DropDownBtn label="Project">
     <template v-slot:drop-content>
-      <q-list
-        dense
-        bordered
-        padding
-        class="rounded-borders"
-        style="width: 300px"
-      >
-        <q-item
-          dense
-          clickable
-          v-ripple
-          v-for="project in starredProjects"
-          :key="project.name"
-        >
-          <q-item-section avatar top>
-            <q-avatar square>
-              <img :src="project.icon" />
-            </q-avatar>
-          </q-item-section>
+      <div class="column">
+        <div class="q-mt-md q-mx-md text-grey-7">Starred</div>
+        <q-list padding style="width: 300px">
+          <q-item
+            dense
+            clickable
+            v-ripple
+            v-for="project in starredProjects"
+            :key="project.name"
+            class="q-py-sm"
+          >
+            <q-item-section avatar top style="margin: auto; min-width: 30px">
+              <q-avatar square style="width: 26px; height: 26px">
+                <img :src="project.icon" />
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label lines="1">{{ project.name }}</q-item-label>
-            <q-item-label caption>Software Project</q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label lines="1">{{ project.name }}</q-item-label>
+              <q-item-label caption>Software Project</q-item-label>
+            </q-item-section>
 
-          <q-item-section side>
-            <q-icon dense name="star" color="yellow-8" />
-          </q-item-section>
-        </q-item>
-      </q-list>
+            <q-item-section side>
+              <q-icon
+                dense
+                name="star"
+                color="yellow-8"
+                style="font-size: 14px"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <div class="q-mx-md text-grey-7">Recent</div>
+        <q-list padding style="width: 300px">
+          <q-item
+            dense
+            clickable
+            v-ripple
+            v-for="project in starredProjects"
+            :key="project.name"
+            class="q-py-sm"
+          >
+            <q-item-section avatar top style="margin: auto; min-width: 30px">
+              <q-avatar square style="width: 26px; height: 26px">
+                <img :src="project.icon" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label lines="1">{{ project.name }}</q-item-label>
+              <q-item-label caption>Software Project</q-item-label>
+            </q-item-section>
+
+            <q-item-section side>
+              <q-icon
+                dense
+                name="star"
+                color="yellow-8"
+                style="font-size: 14px"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
     </template>
   </DropDownBtn>
 </template>
