c286cd456a1a7e41b938ee4939f956b433c47a6b
Korimas <himoker@163.com>
2024-12-11 09:56:01 +0800

    update

--- src/components/header/ProjectDropDown.vue
+++ src/components/header/ProjectDropDown.vue
@@ -6,27 +6,23 @@
         bordered
         padding
         class="rounded-borders"
-        style="width: 100%; width: 200px"
+        style="width: 300px"
       >
-        <q-item clickable v-close-popup>
-          <q-item-section avatar>
-            <q-icon name="join_right" />
+        <q-item clickable v-ripple>
+          <q-item-section avatar top>
+            <q-avatar>
+              <img src="/public/icon1.png" />
+            </q-avatar>
           </q-item-section>
-          <q-item-section> Falcon I </q-item-section>
-        </q-item>
 
-        <q-item clickable v-close-popup>
-          <q-item-section avatar>
-            <q-icon name="view_cozy" />
+          <q-item-section>
+            <q-item-label lines="1">Falcon I</q-item-label>
+            <q-item-label caption>Software Project</q-item-label>
           </q-item-section>
-          <q-item-section> Falcon G </q-item-section>
-        </q-item>
 
-        <q-item clickable v-close-popup>
-          <q-item-section avatar>
-            <q-icon name="join_left" />
+          <q-item-section side>
+            <q-icon name="star" color="green" />
           </q-item-section>
-          <q-item-section> Falcon 10K </q-item-section>
         </q-item>
       </q-list>
     </template>
@@ -35,4 +31,11 @@
 
 <script setup lang="ts">
 import DropDownBtn from './DropDownBtn.vue';
+import { ref } from 'vue';
+
+const starredProjects = ref([
+  { name: 'Falcon I', id: 1 },
+  { name: 'Falcon G', id: 2 },
+  { name: 'Falcon 10K', id: 3 },
+]);
 </script>
