d6eeb274051c1e7670da2e73557be3551e3f55e4
Korimas <himoker@163.com>
2024-12-14 23:04:59 +0800

    update

--- lib/service/projects.ts
+++ lib/service/projects.ts
@@ -9,12 +9,16 @@ export class ProjectsHandler extends BaseApiHandler {
   protected async handlePost(req: Request) {
     const payload = await req.json();
 
-    const result = await this.sql`  
+    const result = (await this.sql`  
         INSERT INTO projects (name, key, icon, description)  
         VALUES (${payload.name}, ${payload.key}, ${payload.icon}, ${payload.description})  
         RETURNING *  
-      `;
+      `) as any[];
 
+    // 判断成功
+    if (result.length === 0) {
+      throw new Error('Failed to insert project');
+    }
     return (result as any[])[0];
   }
 
