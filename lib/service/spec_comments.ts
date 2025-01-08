02db212edd0fc17d9e005eb37d363d5f174fb9b0
korimas <himoker@163.com>
2025-01-08 16:47:07 +0800

    update

--- lib/service/spec_comments.ts
+++ lib/service/spec_comments.ts
@@ -5,37 +5,54 @@ import { BaseApiHandler } from './base';
 export class SpecCommentsHandler extends BaseApiHandler {
 
   protected async handleGet(req: Request) {
-    // //get query params
-    // const url = new URL(req.url);
-    // const id = url.searchParams.get('id');
-    // if (id) {
-    //   return SpecCommentDB.getCommentById(this.sql, parseInt(id));
-    // }
+    //get query params
+    const url = new URL(req.url);
+    const id = url.searchParams.get('id');
+    if (id) {
+      const result = await this.sql`SELECT * FROM projects WHERE id = ${id}`;
+      if (!Array.isArray(result) || result.length === 0) {
+        throw new Error(`Project not found: ${id}`);
+      }
+      return result[0];
+    }
 
-    // const specItemId = url.searchParams.get('spec_item_id');
-    // if (specItemId) {
-    //   return SpecCommentDB.getCommentsBySpecItemId(this.sql, parseInt(specItemId));
-    // }
-    return {};
+    const result = await this.sql`SELECT * FROM projects`;
+    return result;
   }
 
   protected async handlePost(req: Request) {
-    // const payload = await req.json();
-    // return SpecCommentDB.createComment(this.sql, payload);
-    return {};
+    const payload = await req.json();
+
+    const result = (await this.sql`  
+        INSERT INTO projects (name, key, icon, description)  
+        VALUES (${payload.name}, ${payload.key}, ${payload.icon}, ${payload.description})  
+        RETURNING *  
+      `) as any[];
+
+    // 判断成功
+    if (result.length === 0) {
+      throw new Error('Failed to insert project');
+    }
+    return (result as any[])[0];
   }
 
   protected async handleDelete(req: Request) {
-    // const payload = await req.json();
+    const payload = await req.json();
 
-    // if (!Array.isArray(payload.ids) || payload.ids.length === 0) {
-    //   throw new Error('Invalid ids array');
-    // }
+    if (!Array.isArray(payload.ids) || payload.ids.length === 0) {
+      throw new Error('Invalid ids array');
+    }
 
-    // return await SpecCommentDB.deleteComment(this.sql, payload.ids);
+    const result = await this.sql`  
+        DELETE FROM projects  
+        WHERE id = ANY(${payload.ids}::uuid[])  
+        RETURNING id  
+      `;
+
+    return result;
   }
 
   protected async handlePut(req: Request) {
-    return {};
+    return
   }
 }
