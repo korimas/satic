00da92c988feef7e5f83b7de2463818f501fa662
korimas <himoker@163.com>
2025-01-08 16:31:33 +0800

    update

--- api/spec_comments.ts
+++ api/spec_comments.ts
@@ -1,12 +1,12 @@
-// import { SpecCommentsHandler } from "app/lib/service/spec_comments";
-// export const config = {
-//   runtime: 'edge',
-//   regions: ['sin1', 'iad1'],
-// };
+import { SpecCommentsHandler } from "app/lib/service/spec_comments";
+export const config = {
+  runtime: 'edge',
+  regions: ['sin1', 'iad1'],
+};
 
-// const handler = async (req: Request): Promise<Response> => {
-//   const handler = new SpecCommentsHandler();
-//   return handler.handleRequest(req);
-// };
+const handler = async (req: Request): Promise<Response> => {
+  const handler = new SpecCommentsHandler();
+  return handler.handleRequest(req);
+};
 
-// export default handler;
+export default handler;
--- lib/service/spec_comments.ts
+++ lib/service/spec_comments.ts
@@ -1,37 +1,37 @@
 import { BaseApiHandler } from './base';
-import { SpecCommentDB } from '../db/spec_comments';
+// import { SpecCommentDB } from '../db/spec_comments';
 
 
 export class SpecCommentsHandler extends BaseApiHandler {
 
   protected async handleGet(req: Request) {
-    //get query params
-    const url = new URL(req.url);
-    const id = url.searchParams.get('id');
-    if (id) {
-      return SpecCommentDB.getCommentById(this.sql, parseInt(id));
-    }
+    // //get query params
+    // const url = new URL(req.url);
+    // const id = url.searchParams.get('id');
+    // if (id) {
+    //   return SpecCommentDB.getCommentById(this.sql, parseInt(id));
+    // }
 
-    const specItemId = url.searchParams.get('spec_item_id');
-    if (specItemId) {
-      return SpecCommentDB.getCommentsBySpecItemId(this.sql, parseInt(specItemId));
-    }
+    // const specItemId = url.searchParams.get('spec_item_id');
+    // if (specItemId) {
+    //   return SpecCommentDB.getCommentsBySpecItemId(this.sql, parseInt(specItemId));
+    // }
     return null;
   }
 
   protected async handlePost(req: Request) {
-    const payload = await req.json();
-    return SpecCommentDB.createComment(this.sql, payload);
+    // const payload = await req.json();
+    // return SpecCommentDB.createComment(this.sql, payload);
   }
 
   protected async handleDelete(req: Request) {
-    const payload = await req.json();
+    // const payload = await req.json();
 
-    if (!Array.isArray(payload.ids) || payload.ids.length === 0) {
-      throw new Error('Invalid ids array');
-    }
+    // if (!Array.isArray(payload.ids) || payload.ids.length === 0) {
+    //   throw new Error('Invalid ids array');
+    // }
 
-    return await SpecCommentDB.deleteComment(this.sql, payload.ids);
+    // return await SpecCommentDB.deleteComment(this.sql, payload.ids);
   }
 
   protected async handlePut(req: Request) {
